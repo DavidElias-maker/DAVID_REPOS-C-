322c1dad19844540be22ef0ad80aa38823d243b5
DavidElias-maker <130876677+DavidElias-maker@users.noreply.github.com>
2023-04-19 22:31:31 -0600

    siguiendo con cambios api

--- WebApiTransporte/Controllers/ViajeController.cs
+++ WebApiTransporte/Controllers/ViajeController.cs
@@ -28,13 +28,88 @@ namespace WebApiTransporte.Controllers
         }
 
         [HttpPost]
-        [Route("api/Viaje/{SucursalId}/{ColaboradorId}")]
-        public async Task<ActionResult<Sucursal_Colaborador>> PostViaje(int colaboradorid , int sucursalid)
+        public async Task<ActionResult<Viaje>> PostViaje(Viaje viaje)
         {
-            var SucursalExistente = await _context.sucursal_colaborador.FirstOrDefaultAsync(x => x.ColaboradorId == colaboradorid && x.SucursalId == sucursalid);
 
-            return SucursalExistente;
+
+
+
+            //var DistanciaKm = (from f in _context.viaje
+            //                   where f.ColaboradorId == viaje.Id && f.SucursalId == viaje.SucursalId
+            //                   select f.DistanciaKm).FirstOrDefault();
+
+            var sucursalcolaboradorid = (from f in _context.sucursal_colaborador
+                                         where f.SucursalId == viaje.SucursalId
+                                         select f.Id).FirstOrDefault();
+
+
+            var Tarifa = (from f in _context.transportista
+                          where f.Id == viaje.TransportistaId
+                          select f.Tarifa).FirstOrDefault();
+
+            var sucursalcolaborador = await _context.sucursal_colaborador
+         .FirstOrDefaultAsync(f => f.ColaboradorId == viaje.Id && f.SucursalId == viaje.SucursalId);
+
+            if (sucursalcolaborador != null)
+            {
+                var tableData = await _context.viaje
+                    .Where(x => x.Fecha == viaje.Fecha)
+                    .ToListAsync();
+
+                var totalSum = tableData.Sum(x => x.Total);
+
+                return Ok(new { TotalSum = totalSum });
+            }
+            else
+            {
+                return BadRequest("No matching SucursalColaborador found.");
+            }
+
+
+            //if (existecolaboradorsucursal)
+            //{
+            //    return BadRequest("La sucursal ya existe");
+            //}
+            //else
+            //{
+
+            //}
+
+
+
+            // decimal totalcre = Convert.ToDecimal(DistanciaKm) * Convert.ToDecimal(Tarifa);
+
+
+            //_context.Add(totalSum);
+            //await _context.SaveChangesAsync();
+
+            
+           
         }
+       
+   
 
+
+
+        [HttpPost("ViajeNuevo")]
+        public async Task<ActionResult<Viaje>> PostNuevoViaje(Viaje viaje)
+        {
+
+            try
+            {
+                viaje.Total = 0;
+                _context.Add(viaje);
+                await _context.SaveChangesAsync();
+                return viaje;
+
+                
+
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
     }
 }
