73efef393156a397704fde3830f472b202ae8f52
David Flores <130876677+DavidElias-maker@users.noreply.github.com>
2023-05-06 11:40:52 -0600

    se agrego la api de sucursalbyid

--- WebApiTransporte/Controllers/SucursalController.cs
+++ WebApiTransporte/Controllers/SucursalController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApiTransporte.Dtos;
 using WebApiTransporte.Dtos.SucursalDtos;
@@ -8,15 +9,21 @@ using WebApiTransporte.Services.SucursalServices;
 namespace WebApiTransporte.Controllers
 {
 
+
     [ApiController]
     [Route("api/Sucursal")]
     public class SucursalController : ControllerBase
     {
+
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
         private readonly ISucursalService _SucursalService;
 
-        public SucursalController(ISucursalService SucursalService)
+        public SucursalController(ISucursalService SucursalService, ApplicationDbContext context, IMapper mapper)
         {
             this._SucursalService = SucursalService;
+            this._context = context;
+            this._mapper = mapper;
 
         }
         [HttpPost]
@@ -67,6 +74,30 @@ namespace WebApiTransporte.Controllers
             return BuscarSucursalesColaboradores;
 
         }
+        [HttpGet("SucursalColaboradorById")]
+        public async Task<List<SucursalGetColaboradores>> GetSucursalColaboradorById(int sucursalid)
+        {
+
+
+            var collist = await (from p in _context.sucursal_colaborador
+                                 join ps in _context.sucursal_colaborador on p.Id equals ps.Id
+                                 join pm in _context.sucursal on p.SucursalId equals pm.Id
+                                 join pd in _context.colaborador on p.ColaboradorId equals pd.Id
+                                 where p.SucursalId == sucursalid
+                                 select new SucursalGetColaboradores()
+                                 {
+
+                                     Id = ps.Id,
+                                     Nombre = pm.Nombre,
+                                     NombreCompleto = pd.PrimerNombre + " " + pd.PrimerApellido,
+                                     DistanciaKm = ps.DistanciaKm
+
+                                 }).ToListAsync();
+
+            return collist;
+
+        }
+       
 
     }
 }
---
+++ WebApiTransporte/Models/SucursalGetColaboradores.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApiTransporte.Models
+{
+    public class SucursalGetColaboradores
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string NombreCompleto { get; set; }
+        public decimal DistanciaKm { get; set; }
+
+
+
+    }
+}
